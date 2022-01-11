0fcdbd171e1e17c4344bd14d2548f24c8eec808f
Sandro Martini <sandro.martini@gmail.com>
2022-01-11 18:17:35 +0100

    add initial code and related initial tests

--- log4v.v
+++ log4v.v
@@ -1,5 +1,31 @@
-module main
+module log4v
 
-fn main() {
-	println('Hello World!')
+// reuse some standard definitions from V integrated logger
+import log { Logger, Level }
+
+pub const (
+	version = '0.1'
+)
+
+// Log4v represents a logging object
+pub struct Log4v {
+mut:
+	level         Level
+	name          string
+	// TODO: add formatters, appenders, etc ...
+}
+
+// TODO: add Config, etc .. wip
+
+// TODO: check if rename to 'new' only ... wip
+// new create and return a new Log4v instance
+pub fn new_log4v() Log4v {
+	return Log4v {}
+}
+
+// TODO: add other constructor versions ... wip
+
+// get_level gets the internal logging level.
+pub fn (mut l Log4v) get_level() Level {
+	return l.level
 }
---
+++ log4v_test.v
@@ -0,0 +1,21 @@
+module log4v
+
+// import log4v as log // when using from the repository
+
+fn test_empty() {
+	println(@FN + ' ' + 'first tests on Log4v, to ensure it only compiles')
+    assert true
+}
+
+fn test_new_defaults() {
+	println(@FN + ' ' + 'test creation of a new Log4v instance using all defaults')
+
+	log_struct := Log4v{} // direct instancing the struct
+	assert typeof(log_struct).name == '.Log4v'
+
+	// create using constructor and no arguments
+	log := new_log4v() // when using from the same (current) repository
+	// log := log4v.new_log4v()// when using from the repository
+	assert typeof(log).name == '.Log4v'
+}
+
