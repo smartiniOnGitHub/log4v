8fdda06ea05a851415f086a832e80ae05312af31
Sandro Martini <sandro.martini@gmail.com>
2022-01-12 18:46:17 +0100

    implement some basic features and related tests, but still WIP

--- log4v.v
+++ log4v.v
@@ -1,7 +1,8 @@
 module log4v
 
-// reuse some standard definitions from V integrated logger
-import log { Level }
+// reuse some standard definitions from V integrated log module
+// note that many methods are similar (as much as possible) to those in V integrated log module
+import log { Level, Logger }
 
 pub const (
 	version = '0.1'
@@ -9,23 +10,122 @@ pub const (
 
 // Log4v represents a logging object
 pub struct Log4v {
+	ch    chan string // unbuffered (sync)
+	// ch    chan string{cap: buf_len} // buffered (async)
 mut:
-	level Level
-	name  string
+	level Level = .info
+	name  string = 'log4v'
 	// TODO: add formatters, appenders, etc ...
 }
 
 // TODO: add Config, etc .. wip
 
+// new_log4v_as_logger create and return a new Log4v instance, as a generic Logger implementation
+pub fn 	new_log4v_as_logger() Logger {
+	return Log4v{}
+}
+
 // TODO: check if rename to 'new' only ... wip
-// new create and return a new Log4v instance
-pub fn new_log4v() Log4v {
+// new_log4v create and return a new Log4v instance
+pub fn 	new_log4v() Log4v {
 	return Log4v{}
 }
 
 // TODO: add other constructor versions ... wip
 
-// get_level gets the internal logging level.
+// get_level gets the internal logging level
 pub fn (mut l Log4v) get_level() Level {
 	return l.level
 }
+
+// set_level sets the internal logging to `level`
+pub fn (mut l Log4v) set_level(level Level) {
+	l.level = level
+}
+
+// flush writes the log file content to disk
+pub fn (l Log4v) flush() {
+	// TODO: check if it makes sense here ...
+}
+
+// close closes the log file
+pub fn (l Log4v) close() {
+	defer {
+		l.ch.close()
+	}
+}
+
+// format_message format the given log message `s` and level `level` with the log format set in the logger
+fn (l Log4v) format_message(s string, level Level) string {
+	// TODO: check if use sprintf or similar ...
+	return '$l.name - $l.level - $s'
+}
+
+// send_message writes log message `s` to the log buffer
+// to be consumed by all log appenders
+fn (l Log4v) send_message(s string) {
+	l.ch <- s
+}
+
+// process_logs get log messages from logger channel and send to all log appenders
+// to be called asynchronously
+fn (l Log4v) process_logs() {
+	msg := <- l.ch 
+	println(msg) // temp
+	// TODO: send to all log appenders ...
+}
+
+[noreturn] // TODO: check if remove the panic (and the noreturn) here ... wip
+// fatal logs the given message if `Log.level` is greater than or equal to the `Level.fatal` category
+// Note that this method performs a panic at the end, even if log level is not enabled.
+pub fn (l Log4v) fatal(s string) {
+	println(@FN + ' DEBUG') // temp
+	if int(l.level) >= int(Level.fatal) {
+		msg := l.format_message(s, .fatal)
+		l.send_message(msg)
+	}
+	panic('$l.name: $s')
+}
+
+// error logs the given message if `Log.level` is greater than or equal to the `Level.error` category
+pub fn (l Log4v) error(s string) {
+	println(@FN + ' DEBUG') // temp
+	if int(l.level) < int(Level.error) {
+		return
+	}
+	msg := l.format_message(s, .error)
+	l.send_message(msg)
+}
+
+// warn logs the given message if `Log.level` is greater than or equal to the `Level.warn` category
+pub fn (l Log4v) warn(s string) {
+	println(@FN + ' DEBUG') // temp
+	if int(l.level) < int(Level.warn) {
+		return
+	}
+	msg := l.format_message(s, .warn)
+	l.send_message(msg)
+}
+
+// info logs the given message if `Log.level` is greater than or equal to the `Level.info` category
+pub fn (l Log4v) info(s string) {
+	println(@FN + ' DEBUG') // temp
+	if int(l.level) < int(Level.info) {
+		return
+	}
+	// format the string and send to channel
+	msg := l.format_message(s, .info)
+	l.send_message(msg)
+}
+
+// debug logs the given message if `Log.level` is greater than or equal to the `Level.debug` category
+pub fn (l Log4v) debug(s string) {
+	println(@FN + ' DEBUG') // temp
+	if int(l.level) < int(Level.debug) {
+		return
+	}
+	msg := l.format_message(s, .debug)
+	l.send_message(msg)
+}
+
+// TODO: check if add even an additional 'trace' method, but enable it only if debug is enabled at compile time ... wip
--- log4v_test.v
+++ log4v_test.v
@@ -13,8 +13,31 @@ fn test_new_defaults() {
 	log_struct := Log4v{} // direct instancing the struct
 	assert typeof(log_struct).name == '.Log4v'
 
-	// create using constructor and no arguments
+	// create using factory and no arguments, but it returns a generig Logger
+	log_as_logger := new_log4v_as_logger()
+	assert typeof(log_as_logger).name == 'log.Logger'
+
+	// create using factory and no arguments
 	log := new_log4v() // when using from the same (current) repository
 	// log := log4v.new_log4v()// when using from the repository
 	assert typeof(log).name == '.Log4v'
+}
+
+fn test_logger_flow_simple() {
+	println(@FN + ' ' + 'test a minimal flow/usage of a new Log4v instance using all defaults')
+
+	mut l := new_log4v()
+	lp := go l.process_logs() // start async management of logs output
+	defer { l.close() }
+	// call some log methods, but see output when run this source normally (not as a test)
+	l.info('info message')
+	l.warn('warning message')
+	l.error('error message')
+	l.debug('no output for debug')
+	l.set_level(.debug) // this requires logger instance to be mutable
+	l.debug('debug message now')
+	// TODO: add others like level from tag string, panic, etc ... wip
+
+	// lp.wait() // need to wait // TODO: find the right way, or with a timeout here ... wip
+	assert true
 }
